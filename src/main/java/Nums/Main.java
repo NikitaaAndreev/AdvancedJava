26c7d1abd2dee898f9bf335e1759583d33699603
Nikita Andreev <n.andreev@dunice.com>
2022-07-20 09:22:47 +0300

    Exercise 1

--- src/main/java/Nums/Main.java
+++ src/main/java/Nums/Main.java
@@ -1,6 +1,18 @@
 package Nums;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 2, 3, 4 , 4, 55, 55};
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] == nums[j]) count++;
+            }
+        }
+        System.out.println("Количество пар - " + count);
+    }
 }
